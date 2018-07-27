8d136f68bdd16919695b73b272c2062e69f50613
Ryan <omullan.ryan@gmail.com>
2018-07-27 10:43:55 -0300

    Update handler to send email via ses

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # nodeServerlessContactForm
 A serverless contact form built with node.js
+
+
+To use AWS SES, you will need to [verify your email address in the AWS dashboard](https://docs.aws.amazon.com/ses/latest/DeveloperGuide/verify-email-addresses.html).
--- handler.js
+++ handler.js
@@ -1,16 +1,61 @@
-'use strict';
+const serverless = require("serverless-http")
+const AWS = require("aws-sdk")
+const express = require("express")
+const cors = require("cors")
+const bodyParser = require("body-parser")
 
-module.exports.hello = (event, context, callback) => {
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event,
-    }),
+require('dotenv').load();
+
+const app = express();
+
+if (!AWS.config.region) {
+  AWS.config.update({
+      region: "us-east-1"
+  });
+}
+
+const ses = new AWS.SES();
+
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.post("/", (req, res) => {
+  const name = req.body.name;
+  const email = req.body.email;
+  const message = req.body.message;
+
+  const sender_email = process.env.your_verified_email;
+
+  const emailParams = {
+      Source: sender_email,
+      Destination: {
+          ToAddresses: [sender_email,]
+      },
+      ReplyToAddresses: [email],
+      Message: {
+          Body: {
+              Text: {
+                  Charset: "UTF-8",
+                  Data: `${message}  from  ${req.body.email}`
+              }
+          },
+          Subject: {
+              Charset: "UTF-8",
+              Data: `You Received a Message from ${name}`
+          }
+      }
   };
 
-  callback(null, response);
+  ses.sendEmail(emailParams, (err, data) => {
+      if (err) {
+          res.status(402).send(`${err} ${err.stack}`);
+      }
+      if (data) {
+          res.send(data);
+      }
+  });
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
-};
+});
+
+module.exports.form = serverless(app);
